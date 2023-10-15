50a145175bcc80b322a1bce30dcc35a61b82b009
rydnr <github@acm-sl.org>
2023-10-15 12:30:13 +0200

    Added animations and sounds

--- src/components/SensorGrid.tsx
+++ src/components/SensorGrid.tsx
@@ -25,6 +25,7 @@ interface Sensor {
   name: string;
   status: string;
   timestamp: string;
+  previous: string | null;
 }
 
 interface SensorGridProps {
@@ -45,25 +46,47 @@ const SensorGrid: React.FC<SensorGridProps> = (props) => {
     return () => clearInterval(intervalId);
   });
 
+  // Create a signal to store IDs of sensors that should animate
+  const [animatingSensors, setAnimatingSensors] = createSignal(new Set<string>());
+  const [previousStatusProcessed, setPreviousStatusProcessed] = createSignal(new Map<string, string>());
+
+  // Detect changes in sensor data
+  createEffect(() => {
+    sensors().forEach(sensor => {
+      let previousStatus = previousStatusProcessed().get(sensor.id);
+      if (sensor.previous && sensor.previous != sensor.status && previousStatus != sensor.previous) {
+        animatingSensors().add(sensor.id);
+        const audioElement = new Audio(getAudioFile(sensor.status));
+        audioElement.play();
+        console.log(`${sensor.previous} vs ${sensor.status}`)
+        previousStatusProcessed().set(sensor.id, sensor.previous);
+      }
+    });
+  });
+
   return (
     <>
-    <section class="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-10 gap-4 p-4">
-      {sensors().map(sensor => {
-        const status = sensor.status as TranslationKey;
-        return (
-          <div id={sensor.id} class="bg-white shadow-lg rounded p-4" title={t(safeStatus(status))}>
-            <h2 class="text-x1 font-medium text-center mb-2 h-[50px]">{sensor.name}</h2>
-            <div class={`flex flex-col items-center justify-center ${getBackgroundStatusClass(status)}`}>
-              <div class="pt-4">
-                <p class={`text-center w-auto px-4 py-2 rounded-full shadow hover:shadow-md sensor ${getStatusClass(status)}`}>
-                  <img src={`images/${safeStatus(status)}.png`} alt={t(safeStatus(status))} class="w-20 h-auto"/>
-                </p>
+      <section class="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-10 gap-4 p-4">
+        {sensors().map(sensor => {
+          const status = sensor.status as TranslationKey;
+          const isAnimating = animatingSensors().has(sensor.id);
+          if (isAnimating) {
+            animatingSensors().delete(sensor.id);
+          }
+          return (
+            <div id={sensor.id} class="bg-white shadow-lg rounded p-4" title={t(safeStatus(status))}>
+              <h2 class="text-x1 font-medium text-center mb-2 h-[50px]">{sensor.name}</h2>
+              <div class={`flex flex-col items-center justify-center ${getBackgroundStatusClass(status)}`}>
+                <div class="pt-4 relative">
+                  <p class={`text-center w-auto px-4 py-2 rounded-full shadow hover:shadow-md sensor ${getStatusClass(status)}`}>
+                    <img src={`images/${safeStatus(status)}.png`} alt={t(safeStatus(status))} class={`w-20 h-auto ${isAnimating ? 'grow-shrink' : ''}`}/>
+                  </p>
+                </div>
+                <time class="text-center text-xs w-28 px-3 py-2 text-gray-500 min-h-5">{formatTimeString(sensor.timestamp)}</time>
               </div>
-              <time class="text-center text-xs w-28 px-3 py-2 text-gray-500 min-h-5">{formatTimeString(sensor.timestamp)}</time>
             </div>
-          </div>
         )})}
-    </section>
+      </section>
     </>
   );
 }
@@ -108,7 +131,21 @@ function getBackgroundStatusClass(value: TranslationKey): string {
       return 'bg-gray-100';
   }
 }
-
+function getAudioFile(status: string): string {
+  let result = "";
+  switch (status) {
+    case "empty":
+      result = "/mp3/empty.mp3";
+      break;
+    case "stuck":
+      result = "/mp3/stuck.mp3";
+      break;
+    default:
+      result = "/mp3/ok.mp3";
+      break;
+  }
+  return result;
+}
 // testing stuff
 /*
    function randomStatus() {
